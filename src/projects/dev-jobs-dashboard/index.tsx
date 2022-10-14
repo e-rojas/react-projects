d61870ed4be14513d4860119cbe7fcbf25bcf4d4
Edgar <38365682+e-rojas@users.noreply.github.com>
2022-10-13 22:49:01 -0300

    search bar created

--- src/projects/dev-jobs-dashboard/index.tsx
+++ src/projects/dev-jobs-dashboard/index.tsx
@@ -19,13 +19,35 @@ const DevJobsDashboard: React.FC = () => {
       </header>
       <section className='dev-dashboard__wrapper'>
         <div className='dev-dashboard__search '>
-          <form className='nosubmit'>
+          <div className='w-33 border-rigth'>
+            <form className='nosubmit w-100'>
+              <input
+                type='search'
+                placeholder='Filter by title, companies, expertise..'
+                className='nosubmit'
+              />
+            </form>
+          </div>
+          <div className='w-33 border-rigth'>
+            <form className='location w-100'>
+              <input
+                type='search'
+                placeholder='Filter by location..'
+                className='location'
+              />
+            </form>
+          </div>
+          <div className='wrapper'>
+            {/* create a check with label on the right with title full time only */}
             <input
-              type='search'
-              placeholder='Filter by title, companies, expertise..'
-              className='nosubmit'
+              type='checkbox'
+              id='full-time'
+              name='full-time'
+              value='full-time'
             />
-          </form>
+            <label htmlFor='full-time'>Full Time Only</label>
+            <button className='submit-btn'>Search</button>
+          </div>
         </div>
       </section>
     </div>
