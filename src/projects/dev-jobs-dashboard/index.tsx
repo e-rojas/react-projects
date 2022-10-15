6f1b5df63384cb149711eb29022c7c1c693c752e
Edgar <38365682+e-rojas@users.noreply.github.com>
2022-10-15 05:41:27 -0300

    refactoring toggle switch to its own component

--- src/projects/dev-jobs-dashboard/index.tsx
+++ src/projects/dev-jobs-dashboard/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Toggle from 'react-toggle';
 import './dashboard.css';
 import 'react-toggle/style.css';
 import useSetColorTheme from './useSetColorTheme';
 import SearchBar from './components/SearchBar';
-import { HiMoon } from 'react-icons/hi';
-import { BsFillSunFill } from 'react-icons/bs';
+
+import ToggleSwitch from './components/ToggleSwitch';
 const DevJobsDashboard: React.FC = () => {
   const { theme, toggleTheme } = useSetColorTheme();
 
@@ -13,16 +12,7 @@ const DevJobsDashboard: React.FC = () => {
     <div className='dev-dashboard w-100' data-theme={theme}>
       <header className='dev-dashboard__header'>
         <h1 className='dev-dashboard__title'>devJobs</h1>
-        <div className='toggle__wrapper'>
-          <BsFillSunFill className='m-r' />
-          <Toggle
-            className='custom-classname'
-            defaultChecked={theme === 'dark'}
-            icons={false}
-            onChange={toggleTheme}
-          />
-          <HiMoon className='m-l' />
-        </div>
+        <ToggleSwitch theme={theme} toggleTheme={toggleTheme} />
       </header>
       <section className='dev-dashboard__wrapper'>
         <SearchBar />
