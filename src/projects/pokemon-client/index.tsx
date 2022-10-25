43b1ef481a616593774aea0f65fb12edfcdf0e94
Edgar <38365682+e-rojas@users.noreply.github.com>
2022-10-25 00:09:48 -0300

    title update

--- src/projects/pokemon-client/index.tsx
+++ src/projects/pokemon-client/index.tsx
@@ -10,23 +10,16 @@ const PokemonClient: React.FC = () => {
   const [savedPokemon, setSavedPokemon] = React.useState<Pokemon[]>([]);
   const [isSending, setIsSending] = React.useState(false);
 
-  if (loading) {
-    return <Spinner />;
-  }
-  if (error) {
-    return <div>Something went wrong</div>;
-  }
-
   return (
     <>
-      <h1>Pokemon Client</h1>
+      <h1>Pokemon Project</h1>
       <div className='grid w-100'>
         <div className='pokemon-api grid-col-span-2 p-3 border-rigth flex-column'>
-          <span className='font-weight-bold'>Pokemon API</span>
-          {pokemonData ? (
+          <span className='font-weight-bold mb-4'>Pokemon API</span>
+          {loading && <Spinner />}
+          {error && <div>Something went wrong</div>}
+          {pokemonData && (
             <Table pokemonData={pokemonData} saveToDB={saveToDB} />
-          ) : (
-            <Spinner />
           )}
         </div>
         <div className='pokemon-api grid-col-span-2 p-3 flex-column '>
