7de3f73052fa3708e128c48693431878f068794d
Edgar <38365682+e-rojas@users.noreply.github.com>
2022-11-14 10:49:04 -0400

    default button component created

---
+++ src/projects/invoice/components/Buttons.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import { BiPlus } from 'react-icons/bi';
+interface Props {
+  title: string;
+  iconDisplay?: boolean;
+  className?: string;
+  handleOnClick?: () => void;
+}
+
+export const DefaultButton: FC<Props> = ({
+  title,
+  iconDisplay,
+  className = 'btn__primary',
+  handleOnClick,
+}) => {
+  return (
+    <button onClick={handleOnClick} className={`invoice-btn ${className}`}>
+      {iconDisplay && (
+        <div>
+          <BiPlus size={20} className='invoice-txt-purple' />
+        </div>
+      )}
+      {title}
+    </button>
+  );
+};
--- src/projects/invoice/index.tsx
+++ src/projects/invoice/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { DefaultButton } from './components/Buttons';
 import './styles.css';
 const InvoiceAppProject: React.FC = () => {
   return (
     <div className='w-100  vh-100 '>
       <h1>Invoice App</h1>
+      <DefaultButton title='New Invoice' iconDisplay={false} />
     </div>
   );
 };
