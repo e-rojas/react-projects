f97e19153f5bc63e38014505db828675f060e4f9
Edgar <38365682+e-rojas@users.noreply.github.com>
2022-11-15 21:27:12 -0400

    no invoice component created

--- src/projects/invoice/index.tsx
+++ src/projects/invoice/index.tsx
@@ -5,6 +5,7 @@ import Sidebar from './components/Bar';
 import { Invoice } from './models/Invoice.interface';
 import data from './data.json';
 import './styles.css';
+import NoInvoices from './components/NoInvoices';
 const InvoiceAppProject: React.FC = () => {
   const invoiceData = data as Invoice[];
   const [themeColor, setThemeColor] = React.useState('');
@@ -25,10 +26,13 @@ const InvoiceAppProject: React.FC = () => {
       <div className='w-100 p'>
         <h1>Invoice App</h1>
         <br />
-        {invoices &&
+        {invoices && invoices.length > 0 ? (
           invoices.map((invoice) => (
             <InvoiceCard key={invoice.id} invoice={invoice} />
-          ))}
+          ))
+        ) : (
+          <NoInvoices />
+        )}
       </div>
     </div>
   );
