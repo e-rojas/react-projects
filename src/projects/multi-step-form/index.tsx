810a2c3751a9785cf57e6e2e7d7e49e9460e054e
EJ <38365682+e-rojas@users.noreply.github.com>
2022-12-05 17:24:34 -0500

    side bard selected item component created

--- src/projects/multi-step-form/index.tsx
+++ src/projects/multi-step-form/index.tsx
@@ -2,27 +2,47 @@ import React from 'react';
 import './multi-step-form.styles.scss';
 const MultiStepForm = () => {
   return (
-    <div className='multi-step-form-container w-full h-screen '>
-      <section className='multi-step-form'>
+    <div className='multi-step-form-container w-full '>
+      <section className='multi-step-form sm:w-full md:w-9/12 sm:rounded-none md:rounded-md md:p-4'>
         <aside className='multi-step-form__side-bar'>
-          <div className='selected-btns-wrapper'>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
+          <div className='selected-items-wrapper'>
+            {['Your Info', 'Selected Item', 'Add Ons', 'Summary'].map(
+              (item, index) => (
+                <SideBarSelectedItem
+                  key={index}
+                  itemLabel={item}
+                  itemNumber={index + 1}
+                  selected={index === 3}
+                />
+              )
+            )}
           </div>
         </aside>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit
-          eligendi similique laboriosam blanditiis mollitia illo maiores nobis
-          alias dolore, nihil recusandae qui eum quibusdam dolorem sed
-          veritatis, ea facere reiciendis!
-        </p>
-        <h1 className='sm:my-0 md:my-4'>Hello world</h1>
       </section>
     </div>
   );
 };
 
 export default MultiStepForm;
-/* <h1 className='text-3xl font-bold underline'>Multi-Step-Form</h1>; */
+
+export const SideBarSelectedItem: React.FC<{
+  itemNumber: number;
+  itemLabel: string;
+  selected?: boolean;
+}> = ({ itemNumber, itemLabel, selected }) => {
+  return (
+    <div className='sidebar-selection-item flex items-center space-x-4'>
+      <span
+        className={`selected-item-number rounded-full w-8 h-8  flex justify-center items-center ${
+          selected ? 'selected' : ''
+        }`}
+      >
+        {itemNumber}
+      </span>
+      <div className='selected-item-wrapper flex flex-col'>
+        <span className='selected-item-step '>{`Step ${itemNumber}`}</span>
+        <span className='selected-item-label'>{itemLabel}</span>
+      </div>
+    </div>
+  );
+};
