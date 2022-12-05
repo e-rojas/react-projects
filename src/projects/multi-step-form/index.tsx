a4121af46b6b7ff0b4a0899adc9b6958b8cd0e7c
EJ <38365682+e-rojas@users.noreply.github.com>
2022-12-05 12:44:16 -0500

    base files with tailwind wip

--- src/projects/multi-step-form/index.tsx
+++ src/projects/multi-step-form/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
-import './multi-step-form.styles.css';
+import './multi-step-form.styles.scss';
 const MultiStepForm = () => {
-  return <h1 className='text-3xl font-bold underline'>Multi-Step-Form</h1>;
+  return (
+    <div className='multi-step-form-container w-full h-screen '>
+      <section className='multi-step-form'>
+        <aside className='multi-step-form__side-bar'>
+          <div className='selected-btns-wrapper'>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+          </div>
+        </aside>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit
+          eligendi similique laboriosam blanditiis mollitia illo maiores nobis
+          alias dolore, nihil recusandae qui eum quibusdam dolorem sed
+          veritatis, ea facere reiciendis!
+        </p>
+        <h1 className='sm:my-0 md:my-4'>Hello world</h1>
+      </section>
+    </div>
+  );
 };
 
 export default MultiStepForm;
+/* <h1 className='text-3xl font-bold underline'>Multi-Step-Form</h1>; */
