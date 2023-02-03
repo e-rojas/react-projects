7a8807b34a42244ba7cbcfd38926c030130309dc
Edgar <ejroj.dev@gmail.com>
2023-02-02 20:24:45 -0400

    graphql query updated

--- src/pages/BlogDetails.tsx
+++ src/pages/BlogDetails.tsx
@@ -6,14 +6,13 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { renderOptions } from '../utils';
 import Delay from '../components/Delay.animation';
 const BlogDetails: React.FC = () => {
-  const { id } = useParams();
-  const query = `
+    const { id } = useParams();
+    const query = `
     {
         blog(id:"${id}") {
           sys {
             id
           }
-
           title
           content {
             json
@@ -40,24 +39,44 @@ const BlogDetails: React.FC = () => {
       }
     `;
 
-  const { data, loading, error } = useFetchGraphqlQuery<QueryProps>(query);
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error</div>;
+    const { data, loading, error } = useFetchGraphqlQuery<QueryProps>(query);
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error</div>;
 
-  return (
-    <Delay delay={500}>
-      <div className=' w-100 p-3'>
-        {documentToReactComponents(
-          data?.blog.content.json,
-          renderOptions(data?.blog.content)
-        )}
-      </div>
-    </Delay>
-  );
+    return (
+        <Delay delay={500}>
+            <div className=' w-100 p-4'>
+                <section className='grid grid-cols-3'>
+                    <div className='col-span-2 md:col-span-2'>
+                        {data?.blog && (
+                            <div>
+                                <h1>{data?.blog.title}</h1>
+                                <h3>By: {data?.blog.author}</h3>
+                                <h5>
+                                    {new Date(data?.blog.date).toDateString()}
+                                </h5>
+                            </div>
+                        )}
+                    </div>
+                    <div className='col-span-1 flex flex-col justify-center items-center'>
+                        <img
+                            className='rounded'
+                            src={data?.blog.image.url}
+                            alt=''
+                        />
+                    </div>
+                </section>
+                {documentToReactComponents(
+                    data?.blog.content.json,
+                    renderOptions(data?.blog.content),
+                )}
+            </div>
+        </Delay>
+    );
 };
 
 interface QueryProps {
-  blog: Blog;
+    blog: Blog;
 }
 
 export default BlogDetails;
